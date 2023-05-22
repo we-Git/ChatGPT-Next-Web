4111978cd29c6107db1dddce50b704b0d2d65457
haoyongfang <m18258206193@163.com>
2023-05-22 17:26:58 +0800

    去掉余额和apikey输入框

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -135,11 +135,6 @@ const cn = {
       Title: "历史消息长度压缩阈值",
       SubTitle: "当未压缩的历史消息超过该值时，将进行压缩",
     },
-    Token: {
-      Title: "API Key",
-      SubTitle: "使用自己的 Key 可绕过密码访问限制",
-      Placeholder: "OpenAI API Key",
-    },
 
     AccessCode: {
       Title: "访问密码",
