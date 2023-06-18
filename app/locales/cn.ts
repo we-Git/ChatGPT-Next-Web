5a9aab25a1c54159b1a9d7924d7f8f6b62ffa021
m18258206193@163.com <haoyf1230456>
2023-06-18 20:33:14 +0800

    修改问候信息

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -204,7 +204,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
+    BotHello: "添加微信：**wechat_yf**,备注**密码**，免费获取密码",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
